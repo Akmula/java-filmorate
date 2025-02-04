bc531a54e7dac1642d48ad6779dd9824d997b8ca
Akmula <akmula@yandex.ru>
2025-02-04 11:14:17 +0300

    ТЗ 13 изменить главную ветку (вывод фильмов одним запросом)

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmGenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmGenreRepository.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.dal;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -77,7 +78,7 @@ public class FilmGenreRepository extends BaseRepository<FilmGenre> implements Fi
         jdbcTemplate.batchUpdate(ADD_FILM_GENRE_QUERY, new BatchPreparedStatementSetter() {
 
             @Override
-            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+            public void setValues(@NonNull PreparedStatement preparedStatement, int i) throws SQLException {
                 preparedStatement.setInt(1, filmId);
                 preparedStatement.setInt(2, genreIds.get(i));
             }
